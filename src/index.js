a09fae5a113e173d317dbe7613af383bf73e69c1
Leonardo <fanger53@gmail.com>
2022-02-24 08:59:45 -0500

    add typed animation to home section, change of font and font size

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles/styles.scss';
+import Typed from 'typed.js';
+
+const options = {
+  strings: ['a Full-Stack Developer', 'a Freelancer', 'a Code Lover', 'a Mountain Biker', 'Leo'],
+  typeSpeed: 80,
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
