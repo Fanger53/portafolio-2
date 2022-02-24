1136f8cc770ac8298df4e58abd321e1246c6c58e
Leonardo <fanger53@gmail.com>
2022-02-24 15:25:34 -0500

    add new style to about section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './styles/styles.scss';
-import Typed from 'typed.js';
-
-const options = {
-  strings: ['a Full-Stack Developer', 'a Freelancer', 'a Code Lover', 'a Mountain Biker', 'Leo'],
-  typeSpeed: 80,
-  loop: true
-};
-
-const typed = new Typed('.typed', options);
+import './js/home'
